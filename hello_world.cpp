959e3e32f2c20dd3f76d2c0cdefe59e1b475fe20
creatior <vartem2020@mail.ru>
2024-11-22 21:20:57 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 
+// main block
 int main()
 {
+    // user name
     std::string name;
     std::cin >> name;
+    // hello world
     std::cout << "Hello, world from " << name << std::endl;
     return 0;
 }
